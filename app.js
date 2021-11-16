8ab0e668442fd35e859142944c9776268396caa9
Harvey Francois <har.francois@gmail.com>
2021-11-15 18:15:53 -0800

    added app.js

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ let greeting = prompt("Would you like to shop with us? Yes/No");
 
 console.log(greeting);
 
-if(greeting === "yes"){
+if(greeting.toLowerCase() === "yes"){
     window.confirm("Would you like to see our inventory?");
 }
-else if(greeting === "no"){
+else if(greeting.toLowerCase() === "no"){
     alert("Thanks for Visiting");
 }
 
